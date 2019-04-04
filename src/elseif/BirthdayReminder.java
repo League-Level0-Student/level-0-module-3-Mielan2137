cff303118483a6d38a4b94c31bfc9cb6c3e2406f
ilabstaff <ilabstaff@172.16.116.82>
2019-04-03 19:17:56 -0700

    BirthdayReminder and HighLowGame

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -1,6 +1,4 @@
 
-//    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
 package elseif;
 
@@ -10,22 +8,27 @@ public class BirthdayReminder {
 
 	public static void main(String[] args) {
 	
-		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "June 26th";
+		String dadsBirthday = "June 2nd";
+		String myBirthday = "March 7th";
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
-		// 3. Print out what the user typed
-		
-		// 4. if user asked for "mom"
-			//print mom's birthday
-		// 5. if user asked for "dad"
-			// print dad's birthday
-		// 6. if user asked for your name
-			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
 
+		for (int i = 0; i < 5; i++) {
+			
+		String Birthday = JOptionPane.showInputDialog("Which family members birthday would you like to know?");
+		if (Birthday.equals("Mom")){
+		JOptionPane.showMessageDialog(null, "Mom's Birthday is "+ momsBirthday);
+	}
+		else if (Birthday.equals("Dad")) {
+			JOptionPane.showMessageDialog(null, "Dad's birthday is " + dadsBirthday);
+	}
+		else if (Birthday.equals("Yours")) {
+			JOptionPane.showMessageDialog(null, "My Birthday is " + myBirthday);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "I am not sure who it is you are speaking of");
+			
+	}
+	}
 	} 
-}
+	}
--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -4,33 +4,32 @@
 package elseif;
 
 import java.util.Random;
+import java.util.jar.JarOutputStream;
+
+import javax.swing.JOptionPane;
 
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		
-		// 2. Print out the random variable above
-		
-		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
 
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+		int random = new Random().nextInt(5) + 1;
+		System.out.println(random);
+		for (int i = 0; i < 5; i++) {
+
+				String Guess = JOptionPane.showInputDialog("Give your best numerical guess from 1-5");
+
+				int Answer = Integer.parseInt(Guess);
 			
-			// 5. if the guess is correct
-				// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
-
-		// 13. Tell them they lose
-		
+				if (Answer == random) {
+				JOptionPane.showMessageDialog(null, "You Win");
+				System.exit(0);
+			} 
+				else if (Answer > random) {
+				JOptionPane.showMessageDialog(null, "Your Guess is too high");
+			} 
+				else if (Answer < random) {
+				JOptionPane.showMessageDialog(null, "Your guess is too low");
+			}
+		}
 	}
-
 }
-
-
