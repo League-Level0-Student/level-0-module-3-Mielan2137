918b0af62c6df18859c25d0b35ce948e3fca8a80
ilabstaff <ilabstaff@172.16.116.81>
2019-04-17 18:49:05 -0700

    FizzBuzz

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -3,6 +3,7 @@
 
 package modulo;
 
+import javax.swing.JOptionPane;
 
 /**
 * Fizz Buzz
@@ -25,7 +26,25 @@ package modulo;
 * 
 **/
 
+
+
+
 public class FizzBuzz {
-
-}
-
+ public static void main(String[] args) {
+	 	
+	 	for (int i=0; i<=20;i++) {
+		
+		if (i%3==0&&i%5==0) {
+			System.out.println("FizzBuzz");
+	}
+		else if (i%5==0){
+			System.out.println("Buzz");
+	}
+		else if (i%3==0) {
+			System.out.println("Fizz");
+	}
+		else
+			System.out.println(i);
+	}
+	}
+	}
